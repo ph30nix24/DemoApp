9acd8960f06f57048c5212184c6f5888e95ceeae
Anuj <starlordindia1230@gmail.com>
2024-11-29 12:04:46 +0530

    assstss commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Image1 from '../../src/assets/image1.jpg'
+
 const Home = () => {
   return (
     <div className='h-screen'>
@@ -10,7 +10,6 @@ const Home = () => {
         </div>
         <div className='h-[90%] w-full'>
             <h1 className='text-center text-4xl'>Home pages</h1>
-            <img src={Image1} alt="" />
         </div>
     </div>
   )
