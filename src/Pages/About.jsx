4471bfdf993645cd6ac44620bd238d957c6c616e
Anuj <starlordindia1230@gmail.com>
2024-11-29 13:09:52 +0530

    Nav commit

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import AboutNavBar from '../components/Navbars/AboutNavBar'
 const About = () => {
   return (
     <div className='h-screen'>
-        <div className='h-[10%] flex justify-around text-2xl items-center'>
-            <Link to="/">Home</Link>
-            <Link to="/contact">ContactUs</Link>
-        </div>
-        <h1 className='h-[90%] w-full text-center text-4xl'>About pages</h1>
-        
+        <AboutNavBar/>
     </div>
   )
 }
--- src/Pages/Contect.jsx
+++ src/Pages/Contect.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import ContactUsNavBar from '../components/Navbars/ContactUsNavBar'
 const ContactUs = () => {
   return (
     <div className='h-screen'>
-        <div className='h-[10%] flex justify-around text-2xl items-center'>
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-        </div>
-        <h1 className='h-[90%] w-full text-center text-4xl'>ContactUs pages</h1>
-        
+        <ContactUsNavBar/>
     </div>
   )
 }
